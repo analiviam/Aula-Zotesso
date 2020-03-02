8f75cd1714ecc4b97368dc21d3ee5c16d81f4c2c
Aluno <analimendonca@gmail.com>
2020-03-02 09:36:09 -0300

    noticias

---
+++ aula03.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style>
+     table, th, td {
+       border: 2px solid black;
+       border-collapse: collapse;
+     }
+     h3{
+       color: #b30000;
+       font-size: 30px;
+       padding: 10px;
+     }
+
+     img{
+       height:600px;
+     }
+
+</style>
+    <title> Notícias</title>
+  </head>
+  <body>
+    <h1>Portal de notícias</h1>
+    <div class="noticias1">
+      <h3>Robert Down Junior irá interpretar Dolittle</h3>
+      <img src="https://br.web.img3.acsta.net/pictures/19/10/14/16/18/3699651.jpg" alt="">
+      <p>O ator Robert Downey Jr. passou os últimos onze anos no papel de Tony Stark, o Homem de Ferro, super-herói tecnológico que defendeu a humanidade de todos os tipos de vilões. Em "Dolittle", seu primeiro filme após 5 seguidos do universo Marvel, ele embarca em um novo tipo de aventura. Dá vida ao médico inglês que fala com animais, um clássico da literatura várias vezes adaptado para o cinema.</p>
+      <p>Ele conta ao Fantástico que o principal desafio foi contracenar com animais falantes que não existiam na filmagem - foram inseridos através de efeitos especiais. No filme, a fiel conselheira do Doutor Dolittle é Polinesya. Apesar do nome, ela se parece com a nossa Arara Canindé, encontrada na Amazônia brasileira.</p>
+      <p>O filme não diz qual a origem dela. Mesmo assim o ator - que também é o produtor executivo do longa – se animou com a ideia de incluir animais da fauna brasileira numa possível continuação. Downey Jr. diz que está muito orgulhoso por poder passar uma mensagem de respeito aos animais</p>
+      <h6>Escrito em 1/03/2020</h6>
+    </div>
+
+    <div class="noticias2">
+      <h3>Expo Paranavaí</h3>
+      <p>Faltam poucos dias para começar a 49ª edição da Expo Paranavaí, a maior Feira do Agronegócio da  região noroeste do Paraná, e as vendas de ingressos e passaportes para os quatro grandes shows estão a todo vapor.</p>
+      <table style="width:100%">
+  <tr>
+    <th>Shows</th>
+    <th>Dia</th>
+  </tr>
+  <tr>
+    <td>Jorge e Matheus</td>
+    <td>6 de março</td>
+  </tr>
+  <tr>
+    <td> Lauana Prado + Naiara Azevedo</td>
+    <td>7 de março</td>
+  </tr>
+  <tr>
+    <td>Marília Mendonça</td>
+    <td>13 de março</td>
+  </tr>
+   <tr>
+    <td>Zezé Di Camargo e Luciano</td>
+    <td>14 de março</td>
+  </tr>
+
+</table>
+
+      <p>Já estão vendendo o segundo lote de ingressos</p>
+
+    </div>
+
+    <div class="noticias3">
+      <h3>Diversidade além do pop? Artistas aproximam temáticas LGBTs no fado</h3>
+      <p>"No Mercado da Ribeira, há um romance de amor entre o André que é peixeiro e o Chico que é pescado / Sabem todos que lá vão que o André gosta do Chico / Só a mãe dele é que não consente no namorico" são versos de "O Namorico do André", do Fado Bicha.</p>
+      <p>"Fico incomodado porque, enquanto pessoa homossexual, não podia cantar uma história de amor entre dois homens ou não podia simplesmente cantar uma história de amor e dirigir-me a outro homem, teria que me adaptar e cantar para uma mulher", afirma Caçador.</p>
+      <img src="https://s2.glbimg.com/eZEDAgTG5W5LGeMEDBIq0PZawNM=/0x0:960x960/695x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/M/g/zZFPKxS3KHGRWF9BT6DQ/fado-bicha-credito-hermes-de-paula.jpg" alt="">
+    </div>
+
+  </body>
+</html>
